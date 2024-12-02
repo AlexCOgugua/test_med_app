18b60f1c81794d2b80bd3225027451453527f194
Alex <oguguaalexc@gmail.com>
2024-12-02 11:44:06 -0500

    instant-consultation

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Navbar from './Components/Navbar/Navbar';
 import LandingPage from './Components/LandingPage/LandingPage';
 import Sign_Up from './Components/Sign_Up/Sign_Up';
 import Login from './Components/Login/Login';
+import InstantConsultation from './Components/InstantConsultationBooking/InstantConsultation';
 
 // Function component for the main App
 function App() {
@@ -29,6 +30,7 @@ function App() {
             <Route path="/" element={<LandingPage/>}/>
             <Route path="/signup" element={<Sign_Up/>}/>
             <Route path="/login" element={<Login/>}/>
+            <Route path="/instant-consultation" element={<InstantConsultation />} />
           </Routes>
         </BrowserRouter>
     </div>
